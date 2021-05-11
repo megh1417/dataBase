02be8d519a48f572444d818cd286552159007482
megh1417 <73287255+megh1417@users.noreply.github.com>
2021-05-11 20:12:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,18 +2,28 @@
 var database;
 var form,game, player;
 var gameState= 0;
-var playerCount;
+var playerCount=0;
+var allPlayers;
 function setup(){
 
     // create the connection with your database
     database = firebase.database();
     createCanvas(500,500);
-    
+    game = new Game ();
+    game.getState (); 
+    game.start(); 
+     
 
 }
 
 function draw(){
     background("white");
-    
-    drawSprites();
+    if(playerCount=== 4){
+        game.update(1);
+    }
+    if(gameState === 1 ){
+        clear();
+        game.play();
+    }
+    //drawSprites();
 }
